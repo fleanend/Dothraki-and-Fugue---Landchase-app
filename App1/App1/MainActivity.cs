c35d46578a880896f03b6d41be55f3dc44b8ad86
Fleanend <edoardoferrante@hotmail.it>
2016-10-03 13:59:24 +0200

    Commit Test

---
+++ App1/App1/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+
+namespace App1
+{
+    [Activity(Label = "Dothraki and Fugue", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Log.Verbose("prova","Prova");
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
