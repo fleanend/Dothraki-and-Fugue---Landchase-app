93e9aa7aa7828b60ece6d57bee61aee9727b77c2
Federico D'Ambrosio <fedexist@gmail.com>
2016-10-03 13:32:34 +0200

    Xamarin project started

---
+++ Dothraki and Fugue/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Dothraki_and_Fugue {
+    [Activity(Label = "Dothraki_and_Fugue", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+        protected override void OnCreate( Bundle bundle ) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
