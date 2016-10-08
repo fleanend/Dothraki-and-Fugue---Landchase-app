2688b21bb7a3c12bef600c238234b435956c5c95
Fleanend <edoardoferrante@hotmail.it>
2016-10-08 21:34:12 +0200

    Primo commit serio

--- Dothraki and Fugue/MainActivity.cs
+++ Dothraki and Fugue/MainActivity.cs
@@ -1,15 +1,20 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
 
 namespace Dothraki_and_Fugue {
-    [Activity(Label = "Dothraki_and_Fugue", MainLauncher = true, Icon = "@drawable/icon")]
+   [Activity(Label = "Dothraki_and_Fugue", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : Activity {
         protected override void OnCreate( Bundle bundle ) {
             base.OnCreate(bundle);
-
+            
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+            var imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+            imageView.SetScaleType(ImageView.ScaleType.FitCenter);
+            imageView.SetImageResource(Resource.Drawable.Highgarden);
+
         }
     }
 }
